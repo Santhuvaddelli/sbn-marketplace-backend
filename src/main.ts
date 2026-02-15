a8b1ac5dcd56b2e803e8bff0ce5c7e44a5036c09
Santhuvaddelli <santhuvaddelli13@gmail.com>
2026-02-15 20:04:21 +0530

    Change origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://sbn-frontend.s3-website.eu-north-1.amazonaws.com',
+    origin: 'http://sbn-marketplace.s3-website.eu-north-1.amazonaws.com',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
