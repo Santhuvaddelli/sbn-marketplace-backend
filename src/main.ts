83ab7b3a632f9e16a8685175a74011021acc9dd2
Santhuvaddelli <santhuvaddelli13@gmail.com>
2026-02-16 16:48:18 +0530

    cros update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://sbn-marketplace.s3-website.eu-north-1.amazonaws.com',
+    origin: [
+      'http://sbn-marketplace.s3-website.eu-north-1.amazonaws.com',
+      'https://duhudkcsjq2s9.cloudfront.net',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
