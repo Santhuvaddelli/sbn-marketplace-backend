353b94128e19ee800abe4af80581e48739342fc7
Santhuvaddelli <santhuvaddelli13@gmail.com>
2026-02-16 17:13:34 +0530

    Remove NestJS Cros

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'https://duhudkcsjq2s9.cloudfront.net',
-        'http://sbn-marketplace.s3-website.eu-north-1.amazonaws.com',
-      ];
-
-      // allow non-browser tools (curl, server-to-server)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
-  });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
