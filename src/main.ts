5e54bc1b04385a57690ea587b3d425f3f973de0d
Santhuvaddelli <santhuvaddelli13@gmail.com>
2026-02-16 17:22:55 +0530

    Final Commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://duhudkcsjq2s9.cloudfront.net',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
